ee85ef4e09ab63f7c8aaf6e5ca0f2a6ccb177a33
Sylwia Dobkowska <sylvia.dobkowska@gmail.com>
2024-02-25 12:27:53 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+{
+    const tasks = [
+        {
+            content: "wyrzucić śmieci",
+            done: false,
+        },
+
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        };
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
