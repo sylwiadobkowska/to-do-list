31f18a06144b62e326367162b9e93197cc87c73e
Sylwia Dobkowska <sylvia.dobkowska@gmail.com>
2024-02-25 16:30:39 +0100

    toggle task done function added

--- js/script.js
+++ js/script.js
@@ -15,19 +15,25 @@
         render();
     };
 
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
-                render();
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
     };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
 
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li${task.done ? " style=\"text-decoration:line-through\"" : ""}>
             <button class="js-remove">usuń</button>
             ${task.content}
+            <button class="js-done">zrobione</button>
             </li>
             `;
         };
@@ -38,6 +44,14 @@
         removeButtons.forEach((removeButtons, index) => {
             removeButtons.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
